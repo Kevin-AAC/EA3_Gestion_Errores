2996c76980b5544a702c6b073cfc54bc33b439b5
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-10-03 08:25:41 -0500

    Feat:Metodos clase Usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario {
+    private String nombre;
+    private String cedula;
+    private List<Libro> librosPrestados;
+
+    public Usuario(String nombre,String cedula){
+        this.nombre = nombre;
+        this.cedula=cedula;
+        this.librosPrestados= new ArrayList<>();
+
+    }
+    public void prestarLibro(Libro libro){
+        librosPrestados.add(libro);
+
+    }
+    public void devolverLibro(Libro libro){
+        librosPrestados.remove(libro);
+
+    }
+
+
+
+
+
+
+
 }
