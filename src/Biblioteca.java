4557c903d1096b7e9892eeec4c52aca31f0a2fd7
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-10-03 08:13:28 -0500

    Feat:Metodos clase Biblioteca

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,6 +1,8 @@
 import com.sun.source.doctree.SeeTree;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Biblioteca {
     private String nombre;
@@ -9,9 +11,48 @@ public class Biblioteca {
 
     public Biblioteca(String nombre, List<Usuario> usuarios, List<Libro> catalogo) {
         this.nombre = nombre;
-        this.usuarios = usuarios;
-        this.catalogo = catalogo;
+        this.catalogo = new ArrayList<>();
+        this.usuarios = new ArrayList<>();
     }
 
+    public void agregarLibro(Libro libro){
+        catalogo.add(libro);
+    }
+
+    public List<Libro> buscarPorTitulo(String titulo){
+        return catalogo.stream().filter(libro -> libro.getTitulo().equalsIgnoreCase(titulo))
+                .collect(Collectors.toList());
+
+    }
+
+
+
+    public void agregarUsuario(Usuario usuario){
+        usuarios.add(usuario);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public List<Libro> getCatalogo() {
+        return catalogo;
+    }
+
+    public void setCatalogo(List<Libro> catalogo) {
+        this.catalogo = catalogo;
+    }
 }
 
