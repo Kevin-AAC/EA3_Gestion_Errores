d98fdbc0b994e422ef9987dadf4ca25f1eb83b28
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-10-03 21:17:38 -0500

    Chore:Metodos getter and setter

--- src/Usuario.java
+++ src/Usuario.java
@@ -21,10 +21,19 @@ public class Usuario {
 
     }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public String getCedula() {
+        return cedula;
+    }
 
-
-
-
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
 }
